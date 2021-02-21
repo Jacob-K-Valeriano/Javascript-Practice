d2c071d13321667027043c3177a27486471a9d92
Jacob-Valeriano <jacob.k.valeriano@gmail.com>
2021-02-20 21:59:38 -0600

    If/Else Completed

--- js/if-else.js
+++ js/if-else.js
@@ -13,8 +13,28 @@ var favorite = 'red'; // TODO: change this to your favorite color from the list
 // TODO: When a color is encountered log a message that tells the color, and an object of that color.
 //       Example: Blue is the color of the sky.
 
+if (color === 'red') {
+    console.log("Red is the color of strawberries");
+
+} else if (color === 'orange') {
+    console.log("Orange is the color of my favorite fruit");
+
+} else if (color === 'green') {
+    console.log("Green is the color of grass");
+
+} else if (color === 'blue') {
+    console.log("Blue is the color of the sky");
+
+
+
 // TODO: Have a final else that will catch indigo and violet.
 // TODO: In the else, log: I do not know anything by that color.
 
+} else {
+    console.log("I do not know anything by that color");
+}
+
 // TODO: Using the ternary operator, conditionally log a statement that
 //       says whether the random color matches your favorite color.
+
+(color === favorite) ? console.log(color + " is my favorite color!") : console.log(color + " is not my favorite color")
